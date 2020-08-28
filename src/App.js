73ba96fa2378a8ac48a3a6c852f51ffa9505144e
albertpalacio <you@example.com>
2020-08-27 20:54:26 -0400

    components for toolbar created and basic styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
-// import './App.css';
+import React, {Component} from 'react';
+import Toolbar from './components/Toolbar/Toolbar'
 
-function App() {
-  return    <>  Hello from app!  </>;
- 
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Toolbar />
+        <main style={{marginTop: '64px'}}>
+          <p> This is the page content</p>
+        </main>      
+      </div>
+    )
+  }
 }
 
 export default App;
+
